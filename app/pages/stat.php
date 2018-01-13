d1fabd88eb00faaf36cf823a95d02ab11f79831e
discoursproject <discoursproject@gmail.com>
2018-01-13 11:53:06 -0500

    statistics

---
+++ app/pages/stat.php
@@ -0,0 +1,45 @@
+<?php
+if (!is_mod())
+{
+  die("Restricted");
+}
+
+$pdo = pdo();
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<hr>
+<?php
+$i = 0;
+$date = date('Y-m-d H:i:s', strtotime("-$i hour"));
+$beginnig_of_this_hour_str = substr($date, 0, -5)."00:00";
+$beginning_of_this_hour = strtotime($beginnig_of_this_hour_str);
+while ($i < 24)
+{
+	$end = strtotime("-$i hour", $beginning_of_this_hour);
+	$begin = strtotime("-".($i+1)." hour", $beginning_of_this_hour);
+	
+	echo date('Y-m-d H:i:s', $begin);
+	echo " - ";
+	echo date('Y-m-d H:i:s', $end);
+	echo ": ";
+	
+	// count number of posts here
+	$posts = Post::find("$begin < creation_time AND creation_time < $end");
+	
+	echo count($posts);
+	
+	echo "<br>";
+	if (date('H:i:s', $begin) == "00:00:00")
+	{
+		echo "-----------------------<br>";
+	}
+	
+	$i++;
+}
+?>
+<hr>
+<?php
+echo benchmark();
+?>
